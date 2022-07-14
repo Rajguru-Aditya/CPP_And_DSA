cf120f3e644bd64b27b6b1ada33feab4d7eeba0a
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-07-14 22:05:07 +0530

    C++ basics

---
+++ Temporary/hello_world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //---------- Basic Input and output ---------
+    // int a;
+    // int b;
+    // cout << "Enter value of a\n";
+    // cin >> a;
+    // cout << "Enter value of b\n";
+    // cin >> b;
+    // cout << "Addition of a and b: " << a + b;
+
+    // ----- IF Else ----------
+    return 0;
+}
