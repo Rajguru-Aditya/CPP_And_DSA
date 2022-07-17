7943a28334f330a20f7dcccfbd73548082550e30
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-07-17 16:13:49 +0530

    Max and min element in array

--- Basics/arrays.cpp
+++ Basics/arrays.cpp
@@ -11,11 +11,29 @@ int main()
     cout << "Enter array elements" << endl;
     for (int i = 0; i < arrSize; i++)
     {
-        cout << "Element " << i << ": ";
         cin >> myArray[i];
     }
     for (int i = 0; i < sizeof(myArray) / sizeof(int); i++)
     {
         cout << myArray[i] << endl;
     }
+
+    // Max and Min element in array
+    int maxNum = INT16_MIN;
+    int minNum = INT16_MAX;
+
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (myArray[i] > maxNum)
+        {
+            maxNum = myArray[i];
+        }
+        if (myArray[i] < minNum)
+        {
+            minNum = myArray[i];
+        }
+    }
+
+    cout << "Max num: " << maxNum << endl;
+    cout << "Min num: " << minNum << endl;
 }
